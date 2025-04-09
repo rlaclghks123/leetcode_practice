7125e4a2955ec4795ab9b419deafe15ddfdc94bb
rlaclghks123 <rlaclghks123@naver.com>
2025-04-10 08:26:59 +0900

    121 bestTimeToBuyAndSellStock 완료

---
+++ easy/bestTimeToBuyAndSellStock.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+
+var maxProfit = function (prices) {
+  let minPrice = prices[0];
+  let maxProfit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    const profit = prices[i] - minPrice;
+
+    maxProfit = Math.max(maxProfit, profit);
+    minPrice = Math.min(minPrice, prices[i]);
+  }
+
+  return maxProfit;
+};
